dc7a417a9b8c9a641b7f6b91f3146fa5af9bd321
JOMUMWE <muindijoshua032@gmail.com>
2024-02-04 12:52:24 +0300

    added some things to the navbar

--- shazie/client/src/App.jsx
+++ shazie/client/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Flights from './pages/flights'
+import Hotels from './pages/hotels'
 import axios from 'axios'
 import Toaster from 'react-hot-toast'
 
@@ -20,6 +21,8 @@ function App() {
       <Route path='/' element={<Home />}/>
       <Route path='register' element={<Register />}/>
       <Route path='/login' element={<Login />}/>
+      <Route path='/flights' element={<Flights />}/>
+      <Route path='/hotels' element={<Hotels />}/>
     </Routes>
     </>
   )
--- shazie/client/src/components/Navbar.jsx
+++ shazie/client/src/components/Navbar.jsx
@@ -1,11 +1,21 @@
 import { Link  } from "react-router-dom"
+import {FaPlane , FaBed} from 'react-icons/fa6'
+
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
+    <nav className=" w-full pt-5 h-16 text-sm font-semibold text-[#112211] bg-[FFFFFF] shadow-md">
+      <div className=" w-9/12 mx-auto flex flex-row justify-between h-full my-0">
+        <div className="flex flex-nowrap h-full">
+          <Link to='/flights' className=" mr-4 h-full flex flex-nowrap hover:border-b-[#8DD3BB] hover:border-b-[3px]"><FaPlane className="w-5 h-5 mr-2"/>Find Flight</Link>
+          <Link to='/hotels' className=" h-full flex flex-nowrap hover:border-b-[#8DD3BB] hover:border-b-[3px]"><FaBed className="w-5 h-5 mr-2"/>Find Stays</Link>
+        </div>
+        <Link to='/'className="hover:underline">Home</Link>
+        <div>
+          <Link to='/login' className="p-4 mr-3 hover:bg-slate-200 px-5 py-2 rounded">Login</Link>
+          <Link to='/register' className=" bg-[#112211] text-white px-5 py-2 rounded">Sigup</Link>
+        </div>
+      </div>
     </nav>
   )
 }
