f6f1adc19faee181f7d51b1cc2c4b4f611e91187
Lee Mbaabu <117821233+Theoriest@users.noreply.github.com>
2024-02-06 13:25:03 +0300

    added weather app
    added a new model

--- shazie/client/src/App.jsx
+++ shazie/client/src/App.jsx
@@ -6,7 +6,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import axios from 'axios'
 import Toaster from 'react-hot-toast'
-
+import Weather from './weatherApp'
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.withCredentials = true
 
@@ -20,6 +20,7 @@ function App() {
       <Route path='/' element={<Home />}/>
       <Route path='register' element={<Register />}/>
       <Route path='/login' element={<Login />}/>
+      <Route path='/weather' element={<Weather />} />
     </Routes>
     </>
   )
---
+++ shazie/client/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ce5cd84011msh7c9f80286544d9fp1b3402jsnfd810bc7812a",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9097cf146e82a58db29bc79301d1cd34"; // enter your key from openweather API
